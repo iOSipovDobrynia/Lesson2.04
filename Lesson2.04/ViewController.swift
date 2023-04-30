8cbf0c23c4e7df54db9ff8df6b6be552256f1873
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-04-30 17:57:36 +0300

    Initial Commit

---
+++ Lesson2.04/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2.04
+//
+//  Created by Goodwasp on 30.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

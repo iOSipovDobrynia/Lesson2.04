9c6ed4887835984c1f2accd72f495637ff2a2a5b
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-04-30 22:03:22 +0300

    add segments, label and slider

--- Lesson2.04/ViewController.swift
+++ Lesson2.04/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: IBOutlets
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
+        setupSlider()
+        setupMainLabel()
     }
-
-
+    // MARK: IBActions
+    @IBAction func segmentedControlAction() {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "FIRST"
+            mainLabel.textColor = .red
+        case 1:
+            mainLabel.text = "second"
+            mainLabel.textColor = .orange
+        default:
+            mainLabel.text = "ThIrD"
+            mainLabel.textColor = .green
+        }
+    }
+    
+    @IBAction func sliderAction() {
+        mainLabel.text = slider.value.formatted()
+        view.backgroundColor = view.backgroundColor?.withAlphaComponent(CGFloat(slider.value))
+    }
+    
+    // MARK: private methods
+    private func setupMainLabel() {
+        mainLabel.text = slider.value.formatted()
+        mainLabel.font = UIFont.systemFont(ofSize: 35)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+    }
+    
+    private func setupSlider() {
+        slider.value = 1
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .red
+        slider.maximumTrackTintColor = .black
+        slider.thumbTintColor = .white
+    }
 }
 
